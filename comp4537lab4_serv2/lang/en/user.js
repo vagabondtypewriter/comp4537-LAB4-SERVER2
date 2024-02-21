34078a4b8b1522f4dcfb3ff165f41c7f6285f61a
Kiefer Thom <97416146+vagabondtypewriter@users.noreply.github.com>
2024-02-20 20:15:08 -0800

    Add files via upload

---
+++ comp4537lab4_serv2/lang/en/user.js
@@ -0,0 +1,9 @@
+const userStrings = {
+  notFound: "Word '{term}' not found.",
+  endpointNotFound: "Endpoint not found.",
+  wordExists: "Word already exists in the dictionary.",
+  invalidData: "Invalid data. Both word and definition are required.",
+  invalidJSON: "Invalid JSON data."
+};
+
+module.exports = userStrings;
